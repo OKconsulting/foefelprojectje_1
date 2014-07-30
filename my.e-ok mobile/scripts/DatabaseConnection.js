098cfa49d27a76dadf493b918cd18a52c77f7307
MortenMinne <morten.minne@student.howest.be>
2014-07-30 17:31:21 +0200

    LoginFix

--- my.e-ok mobile/scripts/DatabaseConnection.js
+++
@@ -1,9 +0,0 @@
-﻿var url;
-
-if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-    url = "http://api.okc.be/";
-    alert('Online');
-} else {
-    url = "http://localhost:47493/";
-    alert('Local');
-}
