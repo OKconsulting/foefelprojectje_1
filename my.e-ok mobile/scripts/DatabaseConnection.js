7ede3864a69103469504df2172210aed9f98b140
MortenMinne <morten.minne@student.howest.be>
2014-07-30 17:24:32 +0200

    ClientWithAjaxCalls2

---
+++ my.e-ok mobile/scripts/DatabaseConnection.js
@@ -0,0 +1,9 @@
+﻿var url;
+
+if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+    url = "http://api.okc.be/";
+    alert('Online');
+} else {
+    url = "http://localhost:47493/";
+    alert('Local');
+}
