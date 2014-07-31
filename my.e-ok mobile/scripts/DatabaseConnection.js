8a5dfd88e35ec881ad4d2bffeb7d7bcb98a4023c
MortenMinne <morten.minne@student.howest.be>
2014-07-31 08:58:42 +0200

    ChangedApiUrl

--- my.e-ok mobile/scripts/DatabaseConnection.js
+++ my.e-ok mobile/scripts/DatabaseConnection.js
@@ -1,7 +1,7 @@
 ﻿var url;
 
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-    url = "http://api.okc.be/";
+    url = "http://api.okc.be/myeok/";
 } else {
     url = "http://localhost:47493/";
 }
