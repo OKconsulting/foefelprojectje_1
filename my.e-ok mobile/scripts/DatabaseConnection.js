d3fd88fabef1e15932faaa743bedbdcb22e58172
MortenMinne <morten.minne@okc.be>
2014-12-12 15:29:37 +0100

    Update
    Toevoegen van de mogelijkheid om verlof goed of af te keuren.

--- my.e-ok mobile/scripts/DatabaseConnection.js
+++ my.e-ok mobile/scripts/DatabaseConnection.js
@@ -4,5 +4,5 @@ var url;
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
     url = "http://api.okc.be/myeok/";
 } else {
-    url = "http://localhost:47493/";
+    url = "http://api.okc.be/myeok/";//"http://localhost:47493/";
 }
--- my.e-ok mobile/scripts/Vertalen.js
+++ my.e-ok mobile/scripts/Vertalen.js
@@ -2,117 +2,70 @@
 var btnConfirm = 'Bevestigen'
 
 function vertaalTekst(taal) {
-    if (taal == 'nl')
-        vertaalNl();
-    else if (taal == 'fr')
-        vertaalFr();
-    else if (taal == 'en')
-        vertaalEn();
+    vertaalTeksten(taal);
 
     $('.customDatepicker').datepicker("option", "dayNamesMin", dagenKortHuidig);
     $('.customDatepicker').datepicker("option", "dayNames", dagenHuidig);
     $('.customDatepicker').datepicker("option", "monthNames", maandenHuidig);
     //setDteStyle('.customDatepicker');
 }
-// Nederlandstalige vertaling
-function vertaalNl() {
-    dagenKortHuidig = dagenKortNl;
-    dagenHuidig = dagenNl;
-    maandenHuidig = maandenNl;
 
-    $.each($('.lblMenuHome'), function (key, label) { label.innerHTML = 'Verlofsaldo'; });
-    $.each($('.lblMenuVerlof'), function (key, label) { label.innerHTML = 'Verlof aanvragen'; });
-    $.each($('.lblMenuHistoriek'), function (key, label) { label.innerHTML = 'Historiek'; });
-    $.each($('.lblMenuInst'), function (key, label) { label.innerHTML = 'Taalkeuze'; });
-    $.each($('.lblMenuLogout'), function (key, label) { label.innerHTML = 'Uitloggen'; });
+var msgGeenHistoriek = '';
+var msgGeenSaldi = '';
+var msgAlleVeldenInvullen = '';
+var msgGeenAanvragen = '';
+var msgGeenRedenAfkeuren = '';
 
-    $('#txaReden')[0].innerHTML = 'Reden van afwezigheid';
-    $('#lblDatVan')[0].innerHTML = 'Datum van';
-    $('#lblDatTot')[0].innerHTML = 'Datum tot';
-    $('#btnDetailDagen')[0].innerHTML = 'Verder';
-    $('#btnDetailDagen')[0].value = 'Verder';
-    $('#btnDetailDagen')[0].nextSibling.data = 'Verder';
-    btnConfirm = 'Bevestigen';
+var dynamicAantUur = '';
+var dynamicDag = '';
+var dynamicVoormiddag = '';
+var dynamicNamiddag = '';
+var dynamicVanaf = '';
+var dynamicTot = '';
+var dynamicAantDagen = '';
+var dynamicDteAanvraag = '';
+var dynamicBehandelen = '';
+var dynamicPrestatieCode = "";
+var dynamicMinBezetting = '';
+var dynamicAantAanwezig = '';
+var dynamicAantAangevraagd = '';
+var dynamicRedenAanvrBeh = '';
+var dynamicBtnDagenBevestigen = '';
+function vertaalTeksten(taal) {
+    var xml = $.ajax({
+        url: 'xml/Vertalingen.xml',
+        async: false
+    }).responseText;
 
-    $('#tblPCodes tbody tr:nth-child(1) th')[0].innerHTML = 'Verlofrecht';
-    $('#tblPCodes tbody tr:nth-child(2) th')[0].innerHTML = 'Gebruikt';
-    $('#tblPCodes tbody tr:nth-child(3) th')[0].innerHTML = 'Aangevraagd';
-    $('#tblPCodes tbody tr:nth-child(4) th')[0].innerHTML = 'Beschikbaar';
-    $('#tblPCodes tbody tr:nth-child(5) th')[0].innerHTML = 'Beschikbare uren';
-    $('#tblPCodes tbody tr:nth-child(6) th')[0].innerHTML = 'Geldig tot';
+    // Statische teksten wijzigen
+    $(xml).find('Vertaling').each(function () {
+        if ($(this).attr('isButton') == 1)
+            $('.' + $(this).attr('class')).parent().html($(this).find(taal).text());
+        else
+            $('.' + $(this).attr('class')).html($(this).find(taal).text());
+    });
 
-    $('#tblHist tbody tr:nth-child(1) th')[0].innerHTML = 'Van';
-    $('#tblHist tbody tr:nth-child(2) th')[0].innerHTML = 'Tot en met';
-    $('#tblHist tbody tr:nth-child(3) th')[0].innerHTML = 'Aantal dagen';
-    $('#tblHist tbody tr:nth-child(4) th')[0].innerHTML = 'Aangevraagd op';
-    $('#tblHist tbody tr:nth-child(5) th')[0].innerHTML = 'Status';
-    $('#tblHist tbody tr:nth-child(6) th')[0].innerHTML = 'Behandeld door';
-}
-// Franstalige vertaling
-function vertaalFr() {
-    dagenKortHuidig = dagenKortFr;
-    dagenHuidig = dagenFr;
-    maandenHuidig = maandenFr;
+    // Tekst van meldingen wijzigen
+    msgGeenHistoriek = $(xml).find('Melding[class="GeenHistoriek"]').find(taal).text();
+    msgGeenSaldi = $(xml).find('Melding[class="GeenSaldi"]').find(taal).text();
+    msgAlleVeldenInvullen = $(xml).find('Melding[class="AlleVeldenInvullen"]').find(taal).text();
+    msgGeenAanvragen = $(xml).find('Melding[class="GeenAanvragen"]').find(taal).text();
+    msgGeenRedenAfkeuren = $(xml).find('Melding[class="GeenRedenAfkeuren"]').find(taal).text();
 
-    $.each($('.lblMenuHome'), function (key, label) { label.innerHTML = 'Résumé crédits'; });
-    $.each($('.lblMenuVerlof'), function (key, label) { label.innerHTML = 'Demander congé'; });
-    $.each($('.lblMenuHistoriek'), function (key, label) { label.innerHTML = 'Histoire'; });
-    $.each($('.lblMenuInst'), function (key, label) { label.innerHTML = 'Langue'; });
-    $.each($('.lblMenuLogout'), function (key, label) { label.innerHTML = 'Déconnexion'; });
-
-    $('#txaReden')[0].innerHTML = "Raison d'absence";
-    $('#lblDatVan')[0].innerHTML = 'De';
-    $('#lblDatTot')[0].innerHTML = "Jusqu'au";
-    $('#btnDetailDagen')[0].innerHTML = 'Ensuite';
-    $('#btnDetailDagen')[0].value = 'Ensuite';
-    $('#btnDetailDagen')[0].nextSibling.data = 'Ensuite';
-    btnConfirm = 'Confirmer';
-
-    $('#tblPCodes tbody tr:nth-child(1) th')[0].innerHTML = 'Nombre de jours';
-    $('#tblPCodes tbody tr:nth-child(2) th')[0].innerHTML = 'Utilisé';
-    $('#tblPCodes tbody tr:nth-child(3) th')[0].innerHTML = 'Demandé';
-    $('#tblPCodes tbody tr:nth-child(4) th')[0].innerHTML = 'Disponible';
-    $('#tblPCodes tbody tr:nth-child(5) th')[0].innerHTML = 'Heures disponibles';
-    $('#tblPCodes tbody tr:nth-child(6) th')[0].innerHTML = "Valable jusqu'à";
-
-    $('#tblHist tbody tr:nth-child(1) th')[0].innerHTML = 'De';
-    $('#tblHist tbody tr:nth-child(2) th')[0].innerHTML = "Jusqu'au";
-    $('#tblHist tbody tr:nth-child(3) th')[0].innerHTML = 'Nombre de jours';
-    $('#tblHist tbody tr:nth-child(4) th')[0].innerHTML = 'Demandé le';
-    $('#tblHist tbody tr:nth-child(5) th')[0].innerHTML = 'Statut';
-    $('#tblHist tbody tr:nth-child(6) th')[0].innerHTML = 'Traitée par';
-}
-// Engelstalige vertaling
-function vertaalEn() {
-    dagenKortHuidig = dagenKortEn;
-    dagenHuidig = dagenEn;
-    maandenHuidig = maandenEn;
-
-    $.each($('.lblMenuHome'), function (key, label) { label.innerHTML = 'Leave balance'; });
-    $.each($('.lblMenuVerlof'), function (key, label) { label.innerHTML = 'Leave requests'; });
-    $.each($('.lblMenuHistoriek'), function (key, label) { label.innerHTML = 'History'; });
-    $.each($('.lblMenuInst'), function (key, label) { label.innerHTML = 'Language'; });
-    $.each($('.lblMenuLogout'), function (key, label) { label.innerHTML = 'Logout'; });
-
-    $('#txaReden')[0].innerHTML = 'Reason for absence';
-    $('#lblDatVan')[0].innerHTML = 'From';
-    $('#lblDatTot')[0].innerHTML = 'Up to';
-    $('#btnDetailDagen')[0].innerHTML = 'Continue';
-    $('#btnDetailDagen')[0].value = 'Continue';
-    $('#btnDetailDagen')[0].nextSibling.data = 'Continue';
-    btnConfirm = 'Confirm';
-
-    $('#tblPCodes tbody tr:nth-child(1) th')[0].innerHTML = 'Number of days';
-    $('#tblPCodes tbody tr:nth-child(2) th')[0].innerHTML = 'Used';
-    $('#tblPCodes tbody tr:nth-child(3) th')[0].innerHTML = 'Requested';
-    $('#tblPCodes tbody tr:nth-child(4) th')[0].innerHTML = 'Available';
-    $('#tblPCodes tbody tr:nth-child(5) th')[0].innerHTML = 'Hours available';
-    $('#tblPCodes tbody tr:nth-child(6) th')[0].innerHTML = 'Valid until';
-
-    $('#tblHist tbody tr:nth-child(1) th')[0].innerHTML = 'From';
-    $('#tblHist tbody tr:nth-child(2) th')[0].innerHTML = "Up to and including";
-    $('#tblHist tbody tr:nth-child(3) th')[0].innerHTML = 'Number of days';
-    $('#tblHist tbody tr:nth-child(4) th')[0].innerHTML = 'Requested on';
-    $('#tblHist tbody tr:nth-child(5) th')[0].innerHTML = 'Status';
-    $('#tblHist tbody tr:nth-child(6) th')[0].innerHTML = 'Treated by';
+    // Dynamische teksten wijzigen
+    dynamicAantUur = $(xml).find('DynamischeTekst[class="AantalUur"]').find(taal).text();
+    dynamicDag = $(xml).find('DynamischeTekst[class="Dag"]').find(taal).text();
+    dynamicVoormiddag = $(xml).find('DynamischeTekst[class="Voormiddag"]').find(taal).text();
+    dynamicNamiddag = $(xml).find('DynamischeTekst[class="Namiddag"]').find(taal).text();
+    dynamicVanaf = $(xml).find('DynamischeTekst[class="Vanaf"]').find(taal).text();
+    dynamicTot = $(xml).find('DynamischeTekst[class="Tot"]').find(taal).text();
+    dynamicAantDagen = $(xml).find('DynamischeTekst[class="AantDagen"]').find(taal).text();
+    dynamicDteAanvraag = $(xml).find('DynamischeTekst[class="DteAanvraag"]').find(taal).text();
+    dynamicBehandelen = $(xml).find('DynamischeTekst[class="Behandelen"]').find(taal).text();
+    dynamicPrestatieCode = $(xml).find('DynamischeTekst[class="PrestatieCode"]').find(taal).text();
+    dynamicMinBezetting = $(xml).find('DynamischeTekst[class="MinBezetting"]').find(taal).text();
+    dynamicAantAanwezig = $(xml).find('DynamischeTekst[class="AantAanwezig"]').find(taal).text();
+    dynamicAantAangevraagd = $(xml).find('DynamischeTekst[class="AantAangevraagd"]').find(taal).text();
+    dynamicRedenAanvrBeh = $(xml).find('DynamischeTekst[class="RedenAanvrBeh"]').find(taal).text();
+    dynamicBtnDagenBevestigen = $(xml).find('DynamischeTekst[class="btnDagenBevestigen"]').find(taal).text();
 }
