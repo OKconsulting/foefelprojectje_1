8036a034abe6f12a71fa19ef807d82f32099762d
MortenMinne <morten.minne@okc.be>
2015-01-19 15:58:21 +0100

    Update

---
+++ my.e-ok mobile/scripts/GoogleMapScripts.js
@@ -0,0 +1,15 @@
+﻿function initialize() {
+    var mapOptions = {
+        center: { lat: 50.919812134690694, lng: 3.1733993769739754 },
+        zoom: 16
+    }
+
+    var map = new google.maps.Map(document.getElementById('myGoogleMap'), mapOptions);
+
+    var latLng = new google.maps.LatLng('50.919812134690694', '3.1733993769739754');
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: 'OKc'
+    });
+}
