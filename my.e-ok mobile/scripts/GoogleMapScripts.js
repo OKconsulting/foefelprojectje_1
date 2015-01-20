466211ebe7887ea5907fb2fefcf1b805f25d3321
MortenMinne <morten.minne@okc.be>
2015-01-20 09:09:43 +0100

    Update

--- my.e-ok mobile/scripts/GoogleMapScripts.js
+++ my.e-ok mobile/scripts/GoogleMapScripts.js
@@ -1,19 +1,17 @@
-﻿function initialize() {
-    var mapOptions = {
-        center: { lat: 50.919812134690694, lng: 3.1733993769739754 },
-        zoom: 16
+﻿var googleMapInit = false;
+function initialize() {
+    if (!googleMapInit) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng('50.919812134690694', '3.1733993769739754'),
+            map: new google.maps.Map(document.getElementById('myGoogleMap'), {
+                center: { lat: 50.919812134690694, lng: 3.1733993769739754 },
+                zoom: 16
+            }),
+            title: 'OKc'
+        });
+        googleMapInit = true;
     }
-
-    var map = new google.maps.Map(document.getElementById('myGoogleMap'), mapOptions);
-
-    var latLng = new google.maps.LatLng('50.919812134690694', '3.1733993769739754');
-    var marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        title: 'OKc'
-    });
 }
-
 
 var mapHeight = $(document).height() / 100 * 75;
 $('#myGoogleMap').css('height', mapHeight + 'px');
