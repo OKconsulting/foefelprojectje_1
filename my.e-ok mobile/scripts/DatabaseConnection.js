65d4a9554f1d8b43b1f85f5542b4873469291a0d
MortenMinne <morten.minne@okc.be>
2015-02-24 13:21:28 +0100

    Update

--- my.e-ok mobile/scripts/DatabaseConnection.js
+++ my.e-ok mobile/scripts/DatabaseConnection.js
@@ -5,16 +5,3 @@ if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/))
     url = "http://192.168.2.14:50681/"; //"http://api.okc.be/myeok/";
 else
     url = "http://localhost:50681/";
-
-$.ajax({
-    headers: { 'Access-Control-Allow-Origin': "*" },
-    url: url + 'api/user/getApiLocation/?versieNr=14.4.2',
-    cache: false,
-    async: false,
-    type: 'GET',
-    contentType: 'application/json; charset=utf-8',
-    success: function (data) {
-        if (data != '')
-            url = data;
-    }
-});
