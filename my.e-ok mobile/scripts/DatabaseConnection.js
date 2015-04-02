2942f28fd6392a60378ebeb1c6f9364ced42494f
MortenMinne <morten.minne@okc.be>
2015-04-02 10:38:20 +0200

    Update

--- my.e-ok mobile/scripts/DatabaseConnection.js
+++ my.e-ok mobile/scripts/DatabaseConnection.js
@@ -4,9 +4,9 @@ var url;
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
     url = "http://api.okc.be/myeok_2/";
 
-    // "http://api.okc.be/myeok_2/"; -- Nieuwe API
+    // "http://api.okc.be/myeok_2/"; -- Test API
     // "http://192.168.2.11:50681/"; --> Lokaal
-    // "http://api.okc.be/myeok/"; --> Oude API
+    // "http://api.okc.be/myeok/"; --> Live API
 }
 else {
     url = "http://localhost:50681/";
