f3f5681208c4aab39ce6690f12d621c622df2356
MortenMinne <morten.minne@okc.be>
2015-09-25 09:42:02 +0200

    Revert "Update"
    This reverts commit 15457d10b5ff5c12126a4d27269ea1fcaaead471.

---
+++ my.e-ok mobile/platforms/wp8/www/js/DatabaseConnection.js
@@ -0,0 +1,19 @@
+﻿// Bepalen welke url gebruikt moet worden
+var url;
+
+if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+    url = "http://api.okc.be/myeok/";
+
+    /*var vandaag = new Date();
+    if (vandaag >= new Date(2015, 4, 18, 0, 0, 0, 0)) {
+        url = "http://api.okc.be/myeok/";
+    }*/
+
+    // "http://api.okc.be/myeok_2/"; -- Test API
+    // "http://192.168.2.11:50681/"; --> Lokaal via emulator
+    // "http://localhost:50681/"; --> Lokaal zonder emulator
+    // "http://api.okc.be/myeok/"; --> Live API
+}
+else {
+    url = "http://localhost:50681/";
+}
