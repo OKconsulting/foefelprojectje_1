592ef88907dc4a8a2e0cf8220659bb69acece617
MortenMinne <morten.minne@okc.be>
2015-10-23 11:23:55 +0200

    Update

---
+++ platforms/ios/www/js/DatabaseConnection.js
@@ -0,0 +1,14 @@
+﻿// Bepalen welke url gebruikt moet worden
+var url;
+
+if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+
+    url = "http://api.okc.be/myeok/";
+}
+else {
+    url = "http://api.okc.be/myeok_2/";
+}
+// "http://api.okc.be/myeok_2/";
+// "http://192.168.2.125:50681/";
+// "http://localhost:50681/";
+// "http://api.okc.be/myeok/";
