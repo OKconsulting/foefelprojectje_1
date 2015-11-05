a6c0d63476cee0e3ac020e16ec9de91c29f8a50e
MortenMinne <morten.minne@okc.be>
2015-11-05 07:35:06 +0100

    Update

--- platforms/wp8/www/js/DatabaseConnection.js
+++ platforms/wp8/www/js/DatabaseConnection.js
@@ -1,13 +1,16 @@
 ﻿// Bepalen welke url gebruikt moet worden
 var url;
 
-if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+function determineUrl() {
+    if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
 
-    url = "http://api.okc.be/myeok/";
+        url = "http://api.okc.be/myeok/";
+    }
+    else {
+        url = "http://localhost:50681/";
+    }
 }
-else {
-    url = "http://api.okc.be/myeok_2/";
-}
+determineUrl();
 // "http://api.okc.be/myeok_2/";
 // "http://192.168.2.125:50681/";
 // "http://localhost:50681/";
