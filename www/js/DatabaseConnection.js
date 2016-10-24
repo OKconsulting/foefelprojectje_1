97aa1188c4cf0acdac46276342b848c80330f0a6
MortenMinne <morten.minne@okc.be>
2016-10-24 13:03:46 +0200

    Update

--- www/js/DatabaseConnection.js
+++ www/js/DatabaseConnection.js
@@ -11,6 +11,6 @@ function determineUrl() {
 }
 determineUrl();
 // "http://api.okc.be/myeok_2/";        // Test
-// "http://192.168.2.125:50681/";       // Toestel
-// "http://localhost:50681/";           // Lokaal
+// "http://192.168.2.125:58206/";       // Toestel
+// "http://localhost:58206/";           // Lokaal
 // "http://api.okc.be/myeok/";          // Live
