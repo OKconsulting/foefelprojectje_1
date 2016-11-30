eb3bc2b11422d3bbd09222d627b2b313ef22635f
MortenMinne <morten.minne@okc.be>
2016-11-30 14:48:44 +0100

    Update
    Aanpassen van de target API naar de testserver

--- platforms/wp8/www/js/DatabaseConnection.js
+++ platforms/wp8/www/js/DatabaseConnection.js
@@ -3,10 +3,10 @@ var url;
 
 function determineUrl() {
     if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-        url = "http://localhost:50681/";
+        url = "http://api.okc.be/myeok_2/";
     }
     else {
-        url = "http://localhost:50681/";
+        url = "http://api.okc.be/myeok_2/";
     }
 }
 determineUrl();
