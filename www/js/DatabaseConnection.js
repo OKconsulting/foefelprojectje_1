57bfc8a3ff6fa0696954cdacbf54fc6f7038a922
MortenMinne <morten.minne@okc.be>
2017-01-11 15:01:58 +0100

    Update

--- www/js/DatabaseConnection.js
+++ www/js/DatabaseConnection.js
@@ -3,20 +3,20 @@ var url;
 
 function determineUrl() {
     if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-        url = "http://api.okc.be/myeok_2/";
-        if (new Date() >= new Date(2016, 11, 9)) {
+        url = "http://api.okc.be/myeok/";
+        /*if (new Date() >= new Date(2016, 11, 9)) {
             url = "http://api.okc.be/myeok/";
-        }
+        }*/
     }
     else {
-        url = "http://api.okc.be/myeok_2/";
-        if (new Date() >= new Date(2016, 11, 9)) {
+        url = "http://api.okc.be/myeok/";
+        /*if (new Date() >= new Date(2016, 11, 9)) {
             url = "http://api.okc.be/myeok/";
-        }
+        }*/
     }
 }
 determineUrl();
 // "http://api.okc.be/myeok_2/";        // Test
-// "http://192.168.0.175:58206/";       // Toestel
+// "http://192.168.0.175:58212/";       // Toestel
 // "http://localhost:58210/";           // Lokaal
 // "http://api.okc.be/myeok/";          // Live
